819e6559e99d2e11a83a69dfde50732499eed4a6
Jason Marlow <jasonmarlow@gmail.com>
2018-01-21 13:23:42 -0700

    changes to notes

--- 1_setup/index.ts
+++ 1_setup/index.ts
@@ -3,7 +3,7 @@
 
   2. Install ts-lint and typescript npm packages with --save-dev
 
-  3. Set up tslint.json, .gitignore, and .editorconfig files
+  3. Set up tslint.json, .gitignore, and .editorconfig files, install editorconfig-vscode package
 
   4. Write your first line of code - "Hello, world!"
 */
