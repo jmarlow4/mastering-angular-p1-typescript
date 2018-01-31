ae961ccb4e87fdcac160364d0425581df92a12a6
Jason Marlow <jasonmarlow@gmail.com>
2018-01-31 02:15:23 -0700

    work on generics

--- 5_generics/demo.ts
+++ 5_generics/demo.ts
@@ -12,12 +12,3 @@ let typedFunction: <U>(arg: U) => U = returnAny
 console.log(returnAny('Bruce'))
 console.log(returnGeneric<string>('Steve'))
 console.log(typedFunction<number>(1968))
-
-enum Color {Red = 7, Green, Blue}
-let c: string = Color[7]
-
-console.log(c)
-
-let someValue: any = 'this is a string'
-
-let strLength: number = (someValue as string).length
