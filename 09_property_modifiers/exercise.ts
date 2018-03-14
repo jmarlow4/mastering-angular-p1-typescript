ea74dd7fcb07fd32b193ebfd4201921bcebc00da
Jason Marlow <jasonmarlow@gmail.com>
2018-03-13 20:12:04 -0600

    removed trailing whitespace

--- 09_property_modifiers/exercise.ts
+++ 09_property_modifiers/exercise.ts
@@ -1,21 +1,21 @@
 {
   // 1. Create a class called "Insect."
 
-  
+
     // 2. Create the constructor and have it take in a PRIVATE parameter called
     //    "_type" and have it typed to receive a string value.
-    
+
 
     // 3. Create a public getter called "type" that returns the _type property.
     //    Have it typed to return a string value
-    
+
 
     // 4. Create a method called "scurry" that takes a parameter
     //    called "distance" typed to receive a number value.
     //    Have it log a sentence out to the console that uses both
     //    the "_type" property and the method's "distance" argument value
     //    Example: "The ant has scurried 17 inches!"
-    
+
 
   // 5. Create a new instance of Insect and assign it whatever type you like
   //    as it's constructor argument. Assign this new instance to a const
