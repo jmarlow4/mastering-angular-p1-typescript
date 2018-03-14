617063fa5897b53ecd4fb6269339c12392500cec
Jason Marlow <jasonmarlow@gmail.com>
2018-03-13 21:39:00 -0600

    some more cleanup of whitespace

--- 11_composition/exercise.ts
+++ 11_composition/exercise.ts
@@ -2,20 +2,20 @@
   // 1. Create an interface called "Enemy" that has 2 members:
   //    - A string called "weapon"
   //    - A method that returns nothing called "attack"
-  
-  
+
+
   // 2. create a class called "Slime" that IMPLEMENTS the Enemy interface
-  
+
 
     // 3. The Constructor should take in a private readonly parameter property
     //    called "_color" that's typed to receive a string and a private parameter
     //    property called "_weapon" that's also typed to receive a string
-    
-    
+
+
     // 4. Create a getter for the "_weapon" property called "weapon."
     //    This will satisfy the "weapon" member of the Enemy interface
-    
-    
+
+
     // 5. Implement the Enemy interface's "attack" member method. Have it log
     //    a sentence to the console that contains the values "_color" and
     //    "_weapon" properties
