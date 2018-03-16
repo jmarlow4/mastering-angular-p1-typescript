48f1f9450e5da5ffecdac95721c2023f69ecfb44
Jason Marlow <jasonmarlow@gmail.com>
2018-03-15 22:01:11 -0600

    fixed quotes

--- 12_modules/solution.ts
+++ 12_modules/solution.ts
@@ -1,5 +1,5 @@
 // Import the Car class from the "solution_exports" barrel.
-import { Car } from "./solution_exports";
+import { Car } from './solution_exports'
 
 // Create a new instance of Car and construct it with a make of your choice.
 // Assign it to a const
